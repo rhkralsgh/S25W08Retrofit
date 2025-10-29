f286ac973a208a72eaf330311ea62d8c56194cad
rhkralsgh <kmh23937@gmail.com>
2025-10-29 15:01:56 +0900

    🚧Card

--- app/src/main/java/kr/ac/kumoh/s20210053/s25w08retrofit/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20210053/s25w08retrofit/MainActivity.kt
@@ -4,16 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import kr.ac.kumoh.s20210053.s25w08retrofit.model.Song
 import kr.ac.kumoh.s20210053.s25w08retrofit.ui.theme.S25W08RetrofitTheme
@@ -49,7 +62,37 @@ fun MainScreen(viewModel: SongViewModel = viewModel()) {
 
 @Composable
 fun SongList(list: List<Song>) {
-    Column {
-        Text(list.toString())
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        items(list, key = { it.id }) { song ->
+            SongCard(song)
+        }
+    }
+}
+
+@Composable
+fun SongCard(song: Song) {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(vertical = 16.dp)
+        ) {
+            Text(
+                text = song.title,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+            )
+            Spacer(Modifier.height(16.dp))
+            Text(
+                text = song.singer,
+            )
+        }
+        Spacer(Modifier.width(16.dp))
     }
 }
